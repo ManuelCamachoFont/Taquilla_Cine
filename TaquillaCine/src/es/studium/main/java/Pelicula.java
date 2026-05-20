39b393c91e39d902c8d6c3747de0bda9fd0ea65f
Manuel_Camacho_Font <manuelcamachofont@gmail.com>
2026-05-20 09:33:34 +0200

    clase cine

---
+++ TaquillaCine/src/es/studium/main/java/Pelicula.java
@@ -0,0 +1,55 @@
+package es.studium.main.java;
+
+public class Pelicula {
+
+	private int id;
+	private String titulo;
+	private int duracion;
+	private String sinopsis;
+	
+	public Pelicula() {
+		this.id = 0;
+		this.titulo = "";
+		this.duracion = 0;
+		this.sinopsis = "";
+	}
+	
+	public Pelicula(int id, String titulo, int duracion, String sinopsis) {
+		this.id = id;
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.sinopsis = sinopsis;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public String getSinopsis() {
+		return sinopsis;
+	}
+
+	public void setSinopsis(String sinopsis) {
+		this.sinopsis = sinopsis;
+	}
+}
